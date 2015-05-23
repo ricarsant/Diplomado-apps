9661879e403141dda3db8d925c3a750bbc2b3edd
ricarsant <rjgarzon@hotmail.com>
2015-05-23 12:52:06 -0500

    plugins
    plugins

---
+++ lab47/platforms/firefoxos/www/cordova_plugins.js
@@ -0,0 +1,44 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.geolocation/src/firefoxos/GeolocationProxy.js",
+        "id": "org.apache.cordova.geolocation.GeolocationProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.geolocation": "0.3.12"
+}
+// BOTTOM OF METADATA
+});
